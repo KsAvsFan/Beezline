1d401c2103b0acc7811a0bc7cca78368c9b24c09
Jamie Thomason <jamie.thomason@gmail.com>
2012-11-06 22:27:04 -0600

    implemented line wrapping logic, ramps based on variable

---
+++ Constants.h
@@ -0,0 +1,14 @@
+//
+//  Constants.h
+//  TextView
+//
+//  Created by Jamie Thomason on 11/6/12.
+//  Copyright (c) 2012 Jamie Thomason. All rights reserved.
+//
+
+#ifndef TextView_Constants_h
+#define TextView_Constants_h
+
+
+
+#endif
